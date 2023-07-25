e4a712699a49882af131b2d3e77dcc5555d969cf
Vasitito <91189945+Vasitito@users.noreply.github.com>
2023-07-25 11:43:32 +0300

    Update

--- src/tt_bulls_and_cows.v
+++ src/tt_bulls_and_cows.v
@@ -9,50 +9,32 @@ module tt_um_bulls_and_cows (
     input  wire       rst_n
 );
 
-reg [6:0] ui_in_reg_1;
-reg [6:0] ui_in_reg_2;
-reg save_reg;
+assign uio_oe = 0;
+assign uio_out= 0;
+
+reg save_reg_1;
+reg save_reg_2;
 
 always @(posedge clk) begin
     if(!rst_n) begin
-        ui_in_reg_1<=0;
-        ui_in_reg_2<=0;
-        save_reg<=0;
+        save_reg_1<=0;
+        save_reg_2<=0;
     end
     else begin
-        ui_in_reg_1<=ui_in[6:0];
-        ui_in_reg_2<=ui_in_reg_1; 
-        save_reg<=ui_in_reg_2[6];
-    end
-end
-
-reg [5:0] uio_in_reg_1;
-reg [5:0] uio_in_reg_2;
-
-always @(posedge clk) begin
-    if(!rst_n) begin
-        uio_in_reg_1<=0;
-        uio_in_reg_2<=0;
-    end
-    else begin
-        uio_in_reg_1<=uio_in[5:0];
-        uio_in_reg_2<=uio_in_reg_1[5:0]; 
+        save_reg_1<=ui_in[6];
+        save_reg_2<=save_reg_1;
     end
 end
 
 wire save;
-assign save=(~save_reg) & (ui_in_reg_2[6]);
-
-
-assign uio_oe = 0;
-assign uio_out= 0;
+assign save=(~save_reg_2) & save_reg_1;
 
 top_bulls_and_cows tt_b_c(.clk(clk),
                          .rst_n(rst_n),
-                         .inpA(ui_in_reg_2[2:0]),
-                         .inpB(ui_in_reg_2[5:3]),
-                         .inpC(uio_in_reg_2[2:0]),
-                         .inpD(uio_in_reg_2[5:3]),
+                         .inpA(ui_in[2:0]),
+                         .inpB(ui_in[5:3]),
+                         .inpC(uio_in[2:0]),
+                         .inpD(uio_in[5:3]),
                          .save(save),
                          .segment_out(uo_out));
 
